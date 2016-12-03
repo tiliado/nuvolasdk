417f10dde82d55035a3ca5f9575bb87afad0de0a
Jiří Janoušek <janousek.jiri@gmail.com>
2016-12-03 18:06:24 +0100

    Switch to setuptools
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 LONG_DESC = """\
-SDK for building Nuvola Player's web app scripts
+SDK for building Nuvola Player's web app scripts.
+
+Documentation: https://github.com/tiliado/nuvolasdk
 """
 
 setup(
@@ -11,11 +13,14 @@ setup(
 	version = "0.0.1",
 	author = "Jiří Janoušek",
 	author_email = "janousek.jiri@gmail.com",
-	url = "https://tiliado.eu/nuvolaplayer/",
+	url = "https://github.com/tiliado/nuvolasdk",
 	description = "SDK for building Nuvola Player's web app scripts",
+	keywords = 'nuvola sdk development',
 	long_description = LONG_DESC,
+	license = 'BSD',
 	packages = ['nuvolasdk'],
-	package_data = {'nuvolasdk': ['data/*', 'data/template/*']},
+	package_data = {'nuvolasdk': ['data/*', 'data/template/*', 'data/template/src/*']},
+	exclude_package_data = {'nuvolasdk': ['data/template', 'data/template/src']},
 	scripts = ['scripts/nuvolasdk'],
 	classifiers = [
         "Programming Language :: Python :: 3",
