f719cd6c3012e073bc0b73010aceb3437f5216bd
Jiří Janoušek <janousek.jiri@gmail.com>
2018-02-10 16:25:03 +0100

    appdata: update screenshots
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- nuvolasdk/screenshots.py
+++ nuvolasdk/screenshots.py
@@ -1,60 +1,56 @@
 
 
 SCREENSHOTS = [
-	(
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_quick_list_lock.png",
-		"Nuvola provides a tight integration of web apps with Linux desktop environments, e.g. Ubuntu Unity interface."
+    (
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_lock_to_launcher.png",
+        "Lock your favorite web apps to Unity launcher."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_app_window.png",
-		"Nuvola provides a tight integration of web apps with Linux desktop environments, e.g. GNOME."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_add_to_favorites.png",
+        "Add your favorite web apps to GNOME favorites."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_sound_menu.png",
-		"Media player web apps are integrated with the media player applet situated in the sound menu of Ubuntu Unity interface."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_sound_indicator.png",
+        "Control media player web apps with Unity sound menu."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_mpris_applet.png",
-		"Media player web apps are integrated with the media player applet of GNOME Shell."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_mpris_applet.png",
+        "Control media player web apps with GNOME media player applet."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_media_player_extension_rating.png",
-		"Media player web apps are supported with the Media Player GNOME Shell Extension, including the track rating feature."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_media_player_extension.png",
+        "Control media player web apps with the Media Player GNOME Shell Extension."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_quick_list_pause.png",
-		"Media player web apps add playback controls to the Unity launcher menu."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_quick_list.png",
+        "Media player web apps add playback controls to the Unity launcher menu."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_lyrics.png",
-		"Media player web apps support lyrics fetching."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_lyrics.png",
+        "Media player web apps support lyrics fetching."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_menubar.png",
-		"The global menu bar of Ubuntu Unity interface is fully supported."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_global_menubar.png",
+        "The global menu bar of Ubuntu Unity interface is fully supported."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_untick_show_toolbar.png",
-		"In order to save screen space, it is possible to hide the toolbar."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_toggle_toolbar.png",
+        "In order to save screen space, it is possible to hide the toolbar."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_toolbar_hidden.png",
-		"Main window with a hidden toolbar."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/unity_appindicator.png",
+        "AppIndicators (tray icons) in Ubuntu Unity interface."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_keyboard_shortcuts.png",
-		"It is possible to assign keyboard shortcuts to common actions."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_appindicator.png ",
+        "AppIndicators (tray icons) in GNOME Shell interface."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_features_1.png",
-		"Other features include Tray Icon, Unity Laucher, Notifications, Media keys, Password manager, Audio Scrobbler, ..."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_keyboard_shortcuts.png",
+        "It is possible to assign keyboard shortcuts to common actions."
     ),
     (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/gnome_features_2.png",
-		"(features continued) ... Remote control over HTTP and Lyrics fetching."
-    ),
-    (
-		"https://nuvola.tiliado.eu/screenshots/nuvola/unity_features_1.png",
-		"Other features include Tray Icon, Notifications, Media keys, Password manager, Audio Scrobbler and Remote control over HTTP."
+        "https://nuvola.tiliado.eu/screenshots/nuvola/gnome_features.png",
+        "Enable and configure available features."
     ),
 ]
