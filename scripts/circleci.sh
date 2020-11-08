ab438cd6ff9021f56f527e4d1ea051f174bc5044
Jiří Janoušek <janousek.jiri@gmail.com>
2020-11-08 10:25:48 +0100

    Test docker pull nuvolasdk-ci

--- scripts/circleci.sh
+++ scripts/circleci.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-exec docker run -v `pwd`:/workdir --privileged tiliado/nuvolasdk-ci
+set -x
+docker login -u tiliadopull -p 1855d5dd-fb03-4fd7-8932-d8365b200b6b
+docker pull tiliado/nuvolasdk-ci:circleci
+docker pull tiliado/nuvolasdk-ci:latest
+exec docker run -v `pwd`:/workdir --privileged tiliado/nuvolasdk-ci:latest
